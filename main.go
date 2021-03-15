a6b6126b16373d7f83423f5fb27ac0c198d17b6e
kevin-tolentino <kevin.tolentino95@gmail.com>
2021-03-15 15:48:41 -0700

    completed variadic functions exercise

--- main.go
+++ main.go
@@ -10,6 +10,21 @@ func main() {
 	answer, _ := divide(6, 3) //you should handle the error appropriately
 	fmt.Printf("%f\n", answer)
 
+	total := sum(2.1,3,4,5,6)
+	fmt.Println("!!total", total)
+
+	numbers := []float64{2.1,3,4,5,6}
+	summedSlice := sum(numbers...) //spreading the slice values as arguments for the sum
+	fmt.Printf("total of number %f", summedSlice)
+
+}
+//func to show variadic functions (spread syntax)
+func sum(values ...float64) float64 {
+	total := 0.0
+	for _, value := range values {
+		total += value
+	}
+	return total
 }
 
 func add(p1, p2 float64) float64 {
